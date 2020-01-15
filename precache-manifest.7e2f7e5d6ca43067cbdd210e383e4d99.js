fe291bc4ec61991d1474627d4850aa46cd369047
Julien <goetghebuer.julien@gmail.com>
2020-01-15 14:33:55 +0100

    Updates

---
+++ precache-manifest.7e2f7e5d6ca43067cbdd210e383e4d99.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc53839430bd4162eeb9ad53045344ff",
+    "url": "/Burger-Builder-React/index.html"
+  },
+  {
+    "revision": "39d329574276cf5a8330",
+    "url": "/Burger-Builder-React/static/css/main.ce70a81d.chunk.css"
+  },
+  {
+    "revision": "8df1057af54837601d36",
+    "url": "/Burger-Builder-React/static/js/2.e3f244c2.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Burger-Builder-React/static/js/2.e3f244c2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "39d329574276cf5a8330",
+    "url": "/Burger-Builder-React/static/js/main.2a45cfef.chunk.js"
+  },
+  {
+    "revision": "d0c50433277fea90d08c",
+    "url": "/Burger-Builder-React/static/js/runtime-main.66503f9d.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/Burger-Builder-React/static/media/burger-logo.b8503d26.png"
+  }
+]);
